5b500c260c254e3f2d11e26f119517ea0739dfff
jamesTbaker <jamesTbaker@outlook.com>
2020-02-04 13:48:34 -0500

    Errors

--- src/dev/Services/Errors/index.js
+++ src/dev/Services/Errors/index.js
@@ -4,8 +4,109 @@
  * @description Handles everything related to errors
  */
 
+const Twitter = require('twitter');
+const moment = require('moment');
+
 module.exports = {
-	
+
+	/**
+	 * @name AddErrorToTwitter
+	 * @function
+	 * @async
+	 * @description Given error data, construct Tweet and send it to Twitter.
+	 */
+
+	AddErrorToTwitter: (errorData) =>
+		// return a new promise
+		new Promise((resolve, reject) => {
+			// try to post error to twitter
+			try {
+				// construct tweet based on errorData
+				let errorTweet = moment().format('ddd, MM/DD, h:mm a');
+				errorTweet += ' - ';
+				if (errorData.emergencyError) {
+					errorTweet += 'Emergency Error: ';
+					if (errorData.emergencyErrorDetails) {
+						errorTweet += `${errorData.emergencyErrorDetails}.`;
+					} else {
+						errorTweet += 'No details available.';
+					}
+				} else {
+					errorTweet += 'Standard Error.';
+				}
+				// get a twitter client using neso's twitter settings
+				const twitterClient = new Twitter({
+					consumer_key: process.env.hubHelpTwitterConsumerKey,
+					consumer_secret: process.env.hubHelpTwitterConsumerSecret,
+					access_token_key: process.env.hubHelpTwitterAccessTokenKey,
+					access_token_secret: process.env.hubHelpTwitterAccessTokenSecret,
+				});
+				// attempt post error to Twitter
+				twitterClient.post('statuses/update', {
+					status: errorTweet,
+				}, (tweetingError, tweet, response) => {
+					// if there was an error posting to twitter
+					if (tweetingError) {
+						// construct custom error object
+						const twitterErrorData = {
+							error: true,
+							twitterError: true,
+							twitterErrorDetails: tweetingError,
+						};
+						// resolve the promise with the error data
+						resolve(twitterErrorData);
+					} else {
+						resolve({
+							error: false,
+							twitterError: false,
+						});
+					}
+				});
+			} catch (tweetingError) {
+				// construct custom error object
+				const twitterErrorData = {
+					error: true,
+					twitterError: true,
+					twitterErrorDetails: tweetingError,
+				};
+				// resolve the promise with the error data
+				resolve(twitterErrorData);
+			}
+		}),
+
+	/**
+	 * @name ProcessError
+	 * @function
+	 * @async
+	 * @description Given error data, send data to AddErrorToTwitter(). 
+	 * (This is separate from AddErrorToTwitter() because we may want to 
+	 * process errors in different or additional ways.)
+	 */
+
+	ProcessError: (errorData) =>
+		// return a new promise
+		new Promise((resolve, reject) => {
+			// get a promise to add error to Twitter
+			module.exports.AddErrorToTwitter(errorData)
+				// if the promise is resolved with the result
+				.then((result) => {
+					// resolve this promise with the result
+					resolve(result);
+				})
+				// if the promise is rejected with an error
+				.catch((error) => {
+					// reject this promise with an error
+					reject(error);
+				});
+		}),
+
+	/**
+	 * @name ReturnErrorMessage
+	 * @function
+	 * @description Given a message ID, return an error message.
+	 * @param {number} messageID - ID of message to return, e.g., 14
+	 */
+
 	ReturnErrorMessage: (messageID) => {
 		let messageToReturn = '';
 		switch (messageID) {
